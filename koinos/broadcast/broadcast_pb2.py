039a7c99509c2909e008e045c3aca0f276d7b930
Koinos <67607993+koinos-ci@users.noreply.github.com>
2022-02-23 20:37:25 +0000

    Update for koinos-proto commit 2c8f56d

--- koinos/broadcast/broadcast_pb2.py
+++ koinos/broadcast/broadcast_pb2.py
@@ -22,7 +22,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z6github.com/koinos/koinos-proto-golang/koinos/broadcast',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n koinos/broadcast/broadcast.proto\x12\x10koinos.broadcast\x1a\x13koinos/common.proto\x1a\x14koinos/options.proto\x1a\x1ekoinos/protocol/protocol.proto\"\x94\x01\n\x14transaction_accepted\x12\x31\n\x0btransaction\x18\x01 \x01(\x0b\x32\x1c.koinos.protocol.transaction\x12\x35\n\x07receipt\x18\x02 \x01(\x0b\x32$.koinos.protocol.transaction_receipt\x12\x12\n\x06height\x18\x03 \x01(\x04\x42\x02\x30\x01\"&\n\x12transaction_failed\x12\x10\n\x02id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\"h\n\x0e\x62lock_accepted\x12%\n\x05\x62lock\x18\x01 \x01(\x0b\x32\x16.koinos.protocol.block\x12/\n\x07receipt\x18\x02 \x01(\x0b\x32\x1e.koinos.protocol.block_receipt\">\n\x12\x62lock_irreversible\x12(\n\x08topology\x18\x01 \x01(\x0b\x32\x16.koinos.block_topology\"l\n\nfork_heads\x12\x37\n\x17last_irreversible_block\x18\x01 \x01(\x0b\x32\x16.koinos.block_topology\x12%\n\x05heads\x18\x02 \x03(\x0b\x32\x16.koinos.block_topology\" \n\rgossip_status\x12\x0f\n\x07\x65nabled\x18\x01 \x01(\x08\x42\x38Z6github.com/koinos/koinos-proto-golang/koinos/broadcastb\x06proto3'
+  serialized_pb=b'\n koinos/broadcast/broadcast.proto\x12\x10koinos.broadcast\x1a\x13koinos/common.proto\x1a\x14koinos/options.proto\x1a\x1ekoinos/protocol/protocol.proto\"\x94\x01\n\x14transaction_accepted\x12\x31\n\x0btransaction\x18\x01 \x01(\x0b\x32\x1c.koinos.protocol.transaction\x12\x35\n\x07receipt\x18\x02 \x01(\x0b\x32$.koinos.protocol.transaction_receipt\x12\x12\n\x06height\x18\x03 \x01(\x04\x42\x02\x30\x01\"&\n\x12transaction_failed\x12\x10\n\x02id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\"h\n\x0e\x62lock_accepted\x12%\n\x05\x62lock\x18\x01 \x01(\x0b\x32\x16.koinos.protocol.block\x12/\n\x07receipt\x18\x02 \x01(\x0b\x32\x1e.koinos.protocol.block_receipt\">\n\x12\x62lock_irreversible\x12(\n\x08topology\x18\x01 \x01(\x0b\x32\x16.koinos.block_topology\"l\n\nfork_heads\x12\x37\n\x17last_irreversible_block\x18\x01 \x01(\x0b\x32\x16.koinos.block_topology\x12%\n\x05heads\x18\x02 \x03(\x0b\x32\x16.koinos.block_topology\" \n\rgossip_status\x12\x0f\n\x07\x65nabled\x18\x01 \x01(\x08\"\x98\x01\n\x0c\x65vent_parcel\x12\x16\n\x08\x62lock_id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x03\x12\x0e\n\x06height\x18\x02 \x01(\x04\x12!\n\x0etransaction_id\x18\x03 \x01(\x0c\x42\x04\x80\xb5\x18\x04H\x00\x88\x01\x01\x12*\n\x05\x65vent\x18\x04 \x01(\x0b\x32\x1b.koinos.protocol.event_dataB\x11\n\x0f_transaction_idB8Z6github.com/koinos/koinos-proto-golang/koinos/broadcastb\x06proto3'
   ,
   dependencies=[koinos_dot_common__pb2.DESCRIPTOR,koinos_dot_options__pb2.DESCRIPTOR,koinos_dot_protocol_dot_protocol__pb2.DESCRIPTOR,])
 
@@ -248,6 +248,64 @@ _GOSSIP_STATUS = _descriptor.Descriptor(
   serialized_end=632,
 )
 
+
+_EVENT_PARCEL = _descriptor.Descriptor(
+  name='event_parcel',
+  full_name='koinos.broadcast.event_parcel',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='block_id', full_name='koinos.broadcast.event_parcel.block_id', index=0,
+      number=1, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"",
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=b'\200\265\030\003', file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='height', full_name='koinos.broadcast.event_parcel.height', index=1,
+      number=2, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='transaction_id', full_name='koinos.broadcast.event_parcel.transaction_id', index=2,
+      number=3, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"",
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=b'\200\265\030\004', file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='event', full_name='koinos.broadcast.event_parcel.event', index=3,
+      number=4, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+    _descriptor.OneofDescriptor(
+      name='_transaction_id', full_name='koinos.broadcast.event_parcel._transaction_id',
+      index=0, containing_type=None,
+      create_key=_descriptor._internal_create_key,
+    fields=[]),
+  ],
+  serialized_start=635,
+  serialized_end=787,
+)
+
 _TRANSACTION_ACCEPTED.fields_by_name['transaction'].message_type = koinos_dot_protocol_dot_protocol__pb2._TRANSACTION
 _TRANSACTION_ACCEPTED.fields_by_name['receipt'].message_type = koinos_dot_protocol_dot_protocol__pb2._TRANSACTION_RECEIPT
 _BLOCK_ACCEPTED.fields_by_name['block'].message_type = koinos_dot_protocol_dot_protocol__pb2._BLOCK
@@ -255,12 +313,17 @@ _BLOCK_ACCEPTED.fields_by_name['receipt'].message_type = koinos_dot_protocol_dot
 _BLOCK_IRREVERSIBLE.fields_by_name['topology'].message_type = koinos_dot_common__pb2._BLOCK_TOPOLOGY
 _FORK_HEADS.fields_by_name['last_irreversible_block'].message_type = koinos_dot_common__pb2._BLOCK_TOPOLOGY
 _FORK_HEADS.fields_by_name['heads'].message_type = koinos_dot_common__pb2._BLOCK_TOPOLOGY
+_EVENT_PARCEL.fields_by_name['event'].message_type = koinos_dot_protocol_dot_protocol__pb2._EVENT_DATA
+_EVENT_PARCEL.oneofs_by_name['_transaction_id'].fields.append(
+  _EVENT_PARCEL.fields_by_name['transaction_id'])
+_EVENT_PARCEL.fields_by_name['transaction_id'].containing_oneof = _EVENT_PARCEL.oneofs_by_name['_transaction_id']
 DESCRIPTOR.message_types_by_name['transaction_accepted'] = _TRANSACTION_ACCEPTED
 DESCRIPTOR.message_types_by_name['transaction_failed'] = _TRANSACTION_FAILED
 DESCRIPTOR.message_types_by_name['block_accepted'] = _BLOCK_ACCEPTED
 DESCRIPTOR.message_types_by_name['block_irreversible'] = _BLOCK_IRREVERSIBLE
 DESCRIPTOR.message_types_by_name['fork_heads'] = _FORK_HEADS
 DESCRIPTOR.message_types_by_name['gossip_status'] = _GOSSIP_STATUS
+DESCRIPTOR.message_types_by_name['event_parcel'] = _EVENT_PARCEL
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 transaction_accepted = _reflection.GeneratedProtocolMessageType('transaction_accepted', (_message.Message,), {
@@ -305,8 +368,17 @@ gossip_status = _reflection.GeneratedProtocolMessageType('gossip_status', (_mess
   })
 _sym_db.RegisterMessage(gossip_status)
 
+event_parcel = _reflection.GeneratedProtocolMessageType('event_parcel', (_message.Message,), {
+  'DESCRIPTOR' : _EVENT_PARCEL,
+  '__module__' : 'koinos.broadcast.broadcast_pb2'
+  # @@protoc_insertion_point(class_scope:koinos.broadcast.event_parcel)
+  })
+_sym_db.RegisterMessage(event_parcel)
+
 
 DESCRIPTOR._options = None
 _TRANSACTION_ACCEPTED.fields_by_name['height']._options = None
 _TRANSACTION_FAILED.fields_by_name['id']._options = None
+_EVENT_PARCEL.fields_by_name['block_id']._options = None
+_EVENT_PARCEL.fields_by_name['transaction_id']._options = None
 # @@protoc_insertion_point(module_scope)
