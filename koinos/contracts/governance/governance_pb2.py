25e0e83fd5ea13d9e1f35753b454bc6c8a87e21b
Koinos <67607993+koinos-ci@users.noreply.github.com>
2022-06-09 19:28:26 +0000

    Update for koinos-proto commit fb133bd

--- koinos/contracts/governance/governance_pb2.py
+++ koinos/contracts/governance/governance_pb2.py
@@ -22,7 +22,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'ZAgithub.com/koinos/koinos-proto-golang/koinos/contracts/governance',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n,koinos/contracts/governance/governance.proto\x12\x1bkoinos.contracts.governance\x1a\x14koinos/options.proto\x1a\x1ekoinos/protocol/protocol.proto\"\xfb\x01\n\x0fproposal_record\x12.\n\x08proposal\x18\x01 \x01(\x0b\x32\x1c.koinos.protocol.transaction\x12\x19\n\x11vote_start_height\x18\x02 \x01(\x04\x12\x12\n\nvote_tally\x18\x03 \x01(\x04\x12\x16\n\x0evote_threshold\x18\x04 \x01(\x04\x12\x17\n\x0fshall_authorize\x18\x05 \x01(\x08\x12\x1a\n\x12updates_governance\x18\x06 \x01(\x08\x12<\n\x06status\x18\x07 \x01(\x0e\x32,.koinos.contracts.governance.proposal_status\"X\n\x19submit_proposal_arguments\x12.\n\x08proposal\x18\x01 \x01(\x0b\x32\x1c.koinos.protocol.transaction\x12\x0b\n\x03\x66\x65\x65\x18\x02 \x01(\x04\"\'\n\x16submit_proposal_result\x12\r\n\x05value\x18\x01 \x01(\x08\"9\n\x1cget_proposal_by_id_arguments\x12\x19\n\x0bproposal_id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\"X\n\x19get_proposal_by_id_result\x12;\n\x05value\x18\x01 \x01(\x0b\x32,.koinos.contracts.governance.proposal_record\"\x8e\x01\n!get_proposals_by_status_arguments\x12\x1c\n\x0estart_proposal\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\x12\r\n\x05limit\x18\x02 \x01(\x04\x12<\n\x06status\x18\x03 \x01(\x0e\x32,.koinos.contracts.governance.proposal_status\"]\n\x1eget_proposals_by_status_result\x12;\n\x05value\x18\x01 \x03(\x0b\x32,.koinos.contracts.governance.proposal_record\"F\n\x17get_proposals_arguments\x12\x1c\n\x0estart_proposal\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\x12\r\n\x05limit\x18\x02 \x01(\x04\"S\n\x14get_proposals_result\x12;\n\x05value\x18\x01 \x03(\x0b\x32,.koinos.contracts.governance.proposal_record\"[\n\x19proposal_submission_event\x12>\n\x08proposal\x18\x01 \x01(\x0b\x32,.koinos.contracts.governance.proposal_record\"g\n\x15proposal_status_event\x12\x10\n\x02id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\x12<\n\x06status\x18\x02 \x01(\x0e\x32,.koinos.contracts.governance.proposal_status\"S\n\x13proposal_vote_event\x12\x10\n\x02id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\x12\x12\n\nvote_tally\x18\x02 \x01(\x04\x12\x16\n\x0evote_threshold\x18\x03 \x01(\x04\"\x1a\n\x18\x62lock_callback_arguments\"\x17\n\x15\x62lock_callback_result*R\n\x0fproposal_status\x12\x0b\n\x07pending\x10\x00\x12\n\n\x06\x61\x63tive\x10\x01\x12\x0c\n\x08\x61pproved\x10\x02\x12\x0b\n\x07\x65xpired\x10\x03\x12\x0b\n\x07\x61pplied\x10\x04\x42\x43ZAgithub.com/koinos/koinos-proto-golang/koinos/contracts/governanceb\x06proto3'
+  serialized_pb=b'\n,koinos/contracts/governance/governance.proto\x12\x1bkoinos.contracts.governance\x1a\x14koinos/options.proto\x1a\x1ekoinos/protocol/protocol.proto\"\xa7\x02\n\x0fproposal_record\x12.\n\noperations\x18\x01 \x03(\x0b\x32\x1a.koinos.protocol.operation\x12\x1d\n\x15operation_merkle_root\x18\x02 \x01(\x0c\x12\x19\n\x11vote_start_height\x18\x03 \x01(\x04\x12\x12\n\nvote_tally\x18\x04 \x01(\x04\x12\x16\n\x0evote_threshold\x18\x05 \x01(\x04\x12\x17\n\x0fshall_authorize\x18\x06 \x01(\x08\x12\x1a\n\x12updates_governance\x18\x07 \x01(\x08\x12<\n\x06status\x18\x08 \x01(\x0e\x32,.koinos.contracts.governance.proposal_status\x12\x0b\n\x03\x66\x65\x65\x18\t \x01(\x04\"w\n\x19submit_proposal_arguments\x12.\n\noperations\x18\x01 \x03(\x0b\x32\x1a.koinos.protocol.operation\x12\x1d\n\x15operation_merkle_root\x18\x02 \x01(\x0c\x12\x0b\n\x03\x66\x65\x65\x18\x03 \x01(\x04\"\x18\n\x16submit_proposal_result\"9\n\x1cget_proposal_by_id_arguments\x12\x19\n\x0bproposal_id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\"X\n\x19get_proposal_by_id_result\x12;\n\x05value\x18\x01 \x01(\x0b\x32,.koinos.contracts.governance.proposal_record\"\x8e\x01\n!get_proposals_by_status_arguments\x12\x1c\n\x0estart_proposal\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\x12\r\n\x05limit\x18\x02 \x01(\x04\x12<\n\x06status\x18\x03 \x01(\x0e\x32,.koinos.contracts.governance.proposal_status\"]\n\x1eget_proposals_by_status_result\x12;\n\x05value\x18\x01 \x03(\x0b\x32,.koinos.contracts.governance.proposal_record\"F\n\x17get_proposals_arguments\x12\x1c\n\x0estart_proposal\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\x12\r\n\x05limit\x18\x02 \x01(\x04\"S\n\x14get_proposals_result\x12;\n\x05value\x18\x01 \x03(\x0b\x32,.koinos.contracts.governance.proposal_record\"[\n\x19proposal_submission_event\x12>\n\x08proposal\x18\x01 \x01(\x0b\x32,.koinos.contracts.governance.proposal_record\"g\n\x15proposal_status_event\x12\x10\n\x02id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\x12<\n\x06status\x18\x02 \x01(\x0e\x32,.koinos.contracts.governance.proposal_status\"S\n\x13proposal_vote_event\x12\x10\n\x02id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x04\x12\x12\n\nvote_tally\x18\x02 \x01(\x04\x12\x16\n\x0evote_threshold\x18\x03 \x01(\x04*l\n\x0fproposal_status\x12\x0b\n\x07pending\x10\x00\x12\n\n\x06\x61\x63tive\x10\x01\x12\x0c\n\x08\x61pproved\x10\x02\x12\x0b\n\x07\x65xpired\x10\x03\x12\x0b\n\x07\x61pplied\x10\x04\x12\n\n\x06\x66\x61iled\x10\x05\x12\x0c\n\x08reverted\x10\x06\x42\x43ZAgithub.com/koinos/koinos-proto-golang/koinos/contracts/governanceb\x06proto3'
   ,
   dependencies=[koinos_dot_options__pb2.DESCRIPTOR,koinos_dot_protocol_dot_protocol__pb2.DESCRIPTOR,])
 
@@ -58,11 +58,21 @@ _PROPOSAL_STATUS = _descriptor.EnumDescriptor(
       serialized_options=None,
       type=None,
       create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='failed', index=5, number=5,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='reverted', index=6, number=6,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=1398,
-  serialized_end=1480,
+  serialized_start=1405,
+  serialized_end=1513,
 )
 _sym_db.RegisterEnumDescriptor(_PROPOSAL_STATUS)
 
@@ -72,6 +82,8 @@ active = 1
 approved = 2
 expired = 3
 applied = 4
+failed = 5
+reverted = 6
 
 
 
@@ -84,50 +96,64 @@ _PROPOSAL_RECORD = _descriptor.Descriptor(
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='proposal', full_name='koinos.contracts.governance.proposal_record.proposal', index=0,
-      number=1, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
+      name='operations', full_name='koinos.contracts.governance.proposal_record.operations', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='vote_start_height', full_name='koinos.contracts.governance.proposal_record.vote_start_height', index=1,
-      number=2, type=4, cpp_type=4, label=1,
-      has_default_value=False, default_value=0,
+      name='operation_merkle_root', full_name='koinos.contracts.governance.proposal_record.operation_merkle_root', index=1,
+      number=2, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='vote_tally', full_name='koinos.contracts.governance.proposal_record.vote_tally', index=2,
+      name='vote_start_height', full_name='koinos.contracts.governance.proposal_record.vote_start_height', index=2,
       number=3, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='vote_threshold', full_name='koinos.contracts.governance.proposal_record.vote_threshold', index=3,
+      name='vote_tally', full_name='koinos.contracts.governance.proposal_record.vote_tally', index=3,
       number=4, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='shall_authorize', full_name='koinos.contracts.governance.proposal_record.shall_authorize', index=4,
-      number=5, type=8, cpp_type=7, label=1,
-      has_default_value=False, default_value=False,
+      name='vote_threshold', full_name='koinos.contracts.governance.proposal_record.vote_threshold', index=4,
+      number=5, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='updates_governance', full_name='koinos.contracts.governance.proposal_record.updates_governance', index=5,
+      name='shall_authorize', full_name='koinos.contracts.governance.proposal_record.shall_authorize', index=5,
       number=6, type=8, cpp_type=7, label=1,
       has_default_value=False, default_value=False,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='status', full_name='koinos.contracts.governance.proposal_record.status', index=6,
-      number=7, type=14, cpp_type=8, label=1,
+      name='updates_governance', full_name='koinos.contracts.governance.proposal_record.updates_governance', index=6,
+      number=7, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='status', full_name='koinos.contracts.governance.proposal_record.status', index=7,
+      number=8, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='fee', full_name='koinos.contracts.governance.proposal_record.fee', index=8,
+      number=9, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -145,7 +171,7 @@ _PROPOSAL_RECORD = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=132,
-  serialized_end=383,
+  serialized_end=427,
 )
 
 
@@ -158,15 +184,22 @@ _SUBMIT_PROPOSAL_ARGUMENTS = _descriptor.Descriptor(
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='proposal', full_name='koinos.contracts.governance.submit_proposal_arguments.proposal', index=0,
-      number=1, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
+      name='operations', full_name='koinos.contracts.governance.submit_proposal_arguments.operations', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='fee', full_name='koinos.contracts.governance.submit_proposal_arguments.fee', index=1,
-      number=2, type=4, cpp_type=4, label=1,
+      name='operation_merkle_root', full_name='koinos.contracts.governance.submit_proposal_arguments.operation_merkle_root', index=1,
+      number=2, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"",
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='fee', full_name='koinos.contracts.governance.submit_proposal_arguments.fee', index=2,
+      number=3, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -183,8 +216,8 @@ _SUBMIT_PROPOSAL_ARGUMENTS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=385,
-  serialized_end=473,
+  serialized_start=429,
+  serialized_end=548,
 )
 
 
@@ -196,13 +229,6 @@ _SUBMIT_PROPOSAL_RESULT = _descriptor.Descriptor(
   containing_type=None,
   create_key=_descriptor._internal_create_key,
   fields=[
-    _descriptor.FieldDescriptor(
-      name='value', full_name='koinos.contracts.governance.submit_proposal_result.value', index=0,
-      number=1, type=8, cpp_type=7, label=1,
-      has_default_value=False, default_value=False,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -215,8 +241,8 @@ _SUBMIT_PROPOSAL_RESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=475,
-  serialized_end=514,
+  serialized_start=550,
+  serialized_end=574,
 )
 
 
@@ -247,8 +273,8 @@ _GET_PROPOSAL_BY_ID_ARGUMENTS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=516,
-  serialized_end=573,
+  serialized_start=576,
+  serialized_end=633,
 )
 
 
@@ -279,8 +305,8 @@ _GET_PROPOSAL_BY_ID_RESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=575,
-  serialized_end=663,
+  serialized_start=635,
+  serialized_end=723,
 )
 
 
@@ -325,8 +351,8 @@ _GET_PROPOSALS_BY_STATUS_ARGUMENTS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=666,
-  serialized_end=808,
+  serialized_start=726,
+  serialized_end=868,
 )
 
 
@@ -357,8 +383,8 @@ _GET_PROPOSALS_BY_STATUS_RESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=810,
-  serialized_end=903,
+  serialized_start=870,
+  serialized_end=963,
 )
 
 
@@ -396,8 +422,8 @@ _GET_PROPOSALS_ARGUMENTS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=905,
-  serialized_end=975,
+  serialized_start=965,
+  serialized_end=1035,
 )
 
 
@@ -428,8 +454,8 @@ _GET_PROPOSALS_RESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=977,
-  serialized_end=1060,
+  serialized_start=1037,
+  serialized_end=1120,
 )
 
 
@@ -460,8 +486,8 @@ _PROPOSAL_SUBMISSION_EVENT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1062,
-  serialized_end=1153,
+  serialized_start=1122,
+  serialized_end=1213,
 )
 
 
@@ -499,8 +525,8 @@ _PROPOSAL_STATUS_EVENT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1155,
-  serialized_end=1258,
+  serialized_start=1215,
+  serialized_end=1318,
 )
 
 
@@ -545,63 +571,13 @@ _PROPOSAL_VOTE_EVENT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1260,
-  serialized_end=1343,
+  serialized_start=1320,
+  serialized_end=1403,
 )
 
-
-_BLOCK_CALLBACK_ARGUMENTS = _descriptor.Descriptor(
-  name='block_callback_arguments',
-  full_name='koinos.contracts.governance.block_callback_arguments',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=1345,
-  serialized_end=1371,
-)
-
-
-_BLOCK_CALLBACK_RESULT = _descriptor.Descriptor(
-  name='block_callback_result',
-  full_name='koinos.contracts.governance.block_callback_result',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=1373,
-  serialized_end=1396,
-)
-
-_PROPOSAL_RECORD.fields_by_name['proposal'].message_type = koinos_dot_protocol_dot_protocol__pb2._TRANSACTION
+_PROPOSAL_RECORD.fields_by_name['operations'].message_type = koinos_dot_protocol_dot_protocol__pb2._OPERATION
 _PROPOSAL_RECORD.fields_by_name['status'].enum_type = _PROPOSAL_STATUS
-_SUBMIT_PROPOSAL_ARGUMENTS.fields_by_name['proposal'].message_type = koinos_dot_protocol_dot_protocol__pb2._TRANSACTION
+_SUBMIT_PROPOSAL_ARGUMENTS.fields_by_name['operations'].message_type = koinos_dot_protocol_dot_protocol__pb2._OPERATION
 _GET_PROPOSAL_BY_ID_RESULT.fields_by_name['value'].message_type = _PROPOSAL_RECORD
 _GET_PROPOSALS_BY_STATUS_ARGUMENTS.fields_by_name['status'].enum_type = _PROPOSAL_STATUS
 _GET_PROPOSALS_BY_STATUS_RESULT.fields_by_name['value'].message_type = _PROPOSAL_RECORD
@@ -620,8 +596,6 @@ DESCRIPTOR.message_types_by_name['get_proposals_result'] = _GET_PROPOSALS_RESULT
 DESCRIPTOR.message_types_by_name['proposal_submission_event'] = _PROPOSAL_SUBMISSION_EVENT
 DESCRIPTOR.message_types_by_name['proposal_status_event'] = _PROPOSAL_STATUS_EVENT
 DESCRIPTOR.message_types_by_name['proposal_vote_event'] = _PROPOSAL_VOTE_EVENT
-DESCRIPTOR.message_types_by_name['block_callback_arguments'] = _BLOCK_CALLBACK_ARGUMENTS
-DESCRIPTOR.message_types_by_name['block_callback_result'] = _BLOCK_CALLBACK_RESULT
 DESCRIPTOR.enum_types_by_name['proposal_status'] = _PROPOSAL_STATUS
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -708,20 +682,6 @@ proposal_vote_event = _reflection.GeneratedProtocolMessageType('proposal_vote_ev
   # @@protoc_insertion_point(class_scope:koinos.contracts.governance.proposal_vote_event)
   })
 _sym_db.RegisterMessage(proposal_vote_event)
-
-block_callback_arguments = _reflection.GeneratedProtocolMessageType('block_callback_arguments', (_message.Message,), {
-  'DESCRIPTOR' : _BLOCK_CALLBACK_ARGUMENTS,
-  '__module__' : 'koinos.contracts.governance.governance_pb2'
-  # @@protoc_insertion_point(class_scope:koinos.contracts.governance.block_callback_arguments)
-  })
-_sym_db.RegisterMessage(block_callback_arguments)
-
-block_callback_result = _reflection.GeneratedProtocolMessageType('block_callback_result', (_message.Message,), {
-  'DESCRIPTOR' : _BLOCK_CALLBACK_RESULT,
-  '__module__' : 'koinos.contracts.governance.governance_pb2'
-  # @@protoc_insertion_point(class_scope:koinos.contracts.governance.block_callback_result)
-  })
-_sym_db.RegisterMessage(block_callback_result)
 
 
 DESCRIPTOR._options = None
