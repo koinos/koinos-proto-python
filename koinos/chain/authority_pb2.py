356715c7958fa0fd62f1de0eb41d139f9ef65eee
Koinos <67607993+koinos-ci@users.noreply.github.com>
2022-06-23 19:46:43 +0000

    Update for koinos-proto commit c462198

--- koinos/chain/authority_pb2.py
+++ koinos/chain/authority_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z2github.com/koinos/koinos-proto-golang/koinos/chain',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x1ckoinos/chain/authority.proto\x12\x0ckoinos.chain\x1a\x14koinos/options.proto\"=\n\x0b\x63\x61ll_target\x12\x19\n\x0b\x63ontract_id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x06\x12\x13\n\x0b\x65ntry_point\x18\x02 \x01(\r\"|\n\x13\x61uthorize_arguments\x12.\n\x04type\x18\x01 \x01(\x0e\x32 .koinos.chain.authorization_type\x12,\n\x04\x63\x61ll\x18\x02 \x01(\x0b\x32\x19.koinos.chain.call_targetH\x00\x88\x01\x01\x42\x07\n\x05_call\"!\n\x10\x61uthorize_result\x12\r\n\x05value\x18\x01 \x01(\x08*Y\n\x12\x61uthorization_type\x12\x11\n\rcontract_call\x10\x00\x12\x1b\n\x17transaction_application\x10\x01\x12\x13\n\x0f\x63ontract_upload\x10\x02\x42\x34Z2github.com/koinos/koinos-proto-golang/koinos/chainb\x06proto3'
+  serialized_pb=b'\n\x1ckoinos/chain/authority.proto\x12\x0ckoinos.chain\x1a\x14koinos/options.proto\"Y\n\tcall_data\x12\x19\n\x0b\x63ontract_id\x18\x01 \x01(\x0c\x42\x04\x80\xb5\x18\x06\x12\x13\n\x0b\x65ntry_point\x18\x02 \x01(\r\x12\x0e\n\x06\x63\x61ller\x18\x03 \x01(\x0c\x12\x0c\n\x04\x64\x61ta\x18\x04 \x01(\x0c\"z\n\x13\x61uthorize_arguments\x12.\n\x04type\x18\x01 \x01(\x0e\x32 .koinos.chain.authorization_type\x12*\n\x04\x63\x61ll\x18\x02 \x01(\x0b\x32\x17.koinos.chain.call_dataH\x00\x88\x01\x01\x42\x07\n\x05_call\"!\n\x10\x61uthorize_result\x12\r\n\x05value\x18\x01 \x01(\x08*Y\n\x12\x61uthorization_type\x12\x11\n\rcontract_call\x10\x00\x12\x1b\n\x17transaction_application\x10\x01\x12\x13\n\x0f\x63ontract_upload\x10\x02\x42\x34Z2github.com/koinos/koinos-proto-golang/koinos/chainb\x06proto3'
   ,
   dependencies=[koinos_dot_options__pb2.DESCRIPTOR,])
 
@@ -50,8 +50,8 @@ _AUTHORIZATION_TYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=292,
-  serialized_end=381,
+  serialized_start=318,
+  serialized_end=407,
 )
 _sym_db.RegisterEnumDescriptor(_AUTHORIZATION_TYPE)
 
@@ -62,25 +62,39 @@ contract_upload = 2
 
 
 
-_CALL_TARGET = _descriptor.Descriptor(
-  name='call_target',
-  full_name='koinos.chain.call_target',
+_CALL_DATA = _descriptor.Descriptor(
+  name='call_data',
+  full_name='koinos.chain.call_data',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='contract_id', full_name='koinos.chain.call_target.contract_id', index=0,
+      name='contract_id', full_name='koinos.chain.call_data.contract_id', index=0,
       number=1, type=12, cpp_type=9, label=1,
       has_default_value=False, default_value=b"",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=b'\200\265\030\006', file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='entry_point', full_name='koinos.chain.call_target.entry_point', index=1,
+      name='entry_point', full_name='koinos.chain.call_data.entry_point', index=1,
       number=2, type=13, cpp_type=3, label=1,
       has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='caller', full_name='koinos.chain.call_data.caller', index=2,
+      number=3, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"",
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='data', full_name='koinos.chain.call_data.data', index=3,
+      number=4, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
@@ -97,7 +111,7 @@ _CALL_TARGET = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=68,
-  serialized_end=129,
+  serialized_end=157,
 )
 
 
@@ -140,8 +154,8 @@ _AUTHORIZE_ARGUMENTS = _descriptor.Descriptor(
       create_key=_descriptor._internal_create_key,
     fields=[]),
   ],
-  serialized_start=131,
-  serialized_end=255,
+  serialized_start=159,
+  serialized_end=281,
 )
 
 
@@ -172,27 +186,27 @@ _AUTHORIZE_RESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=257,
-  serialized_end=290,
+  serialized_start=283,
+  serialized_end=316,
 )
 
 _AUTHORIZE_ARGUMENTS.fields_by_name['type'].enum_type = _AUTHORIZATION_TYPE
-_AUTHORIZE_ARGUMENTS.fields_by_name['call'].message_type = _CALL_TARGET
+_AUTHORIZE_ARGUMENTS.fields_by_name['call'].message_type = _CALL_DATA
 _AUTHORIZE_ARGUMENTS.oneofs_by_name['_call'].fields.append(
   _AUTHORIZE_ARGUMENTS.fields_by_name['call'])
 _AUTHORIZE_ARGUMENTS.fields_by_name['call'].containing_oneof = _AUTHORIZE_ARGUMENTS.oneofs_by_name['_call']
-DESCRIPTOR.message_types_by_name['call_target'] = _CALL_TARGET
+DESCRIPTOR.message_types_by_name['call_data'] = _CALL_DATA
 DESCRIPTOR.message_types_by_name['authorize_arguments'] = _AUTHORIZE_ARGUMENTS
 DESCRIPTOR.message_types_by_name['authorize_result'] = _AUTHORIZE_RESULT
 DESCRIPTOR.enum_types_by_name['authorization_type'] = _AUTHORIZATION_TYPE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
-call_target = _reflection.GeneratedProtocolMessageType('call_target', (_message.Message,), {
-  'DESCRIPTOR' : _CALL_TARGET,
+call_data = _reflection.GeneratedProtocolMessageType('call_data', (_message.Message,), {
+  'DESCRIPTOR' : _CALL_DATA,
   '__module__' : 'koinos.chain.authority_pb2'
-  # @@protoc_insertion_point(class_scope:koinos.chain.call_target)
+  # @@protoc_insertion_point(class_scope:koinos.chain.call_data)
   })
-_sym_db.RegisterMessage(call_target)
+_sym_db.RegisterMessage(call_data)
 
 authorize_arguments = _reflection.GeneratedProtocolMessageType('authorize_arguments', (_message.Message,), {
   'DESCRIPTOR' : _AUTHORIZE_ARGUMENTS,
@@ -210,5 +224,5 @@ _sym_db.RegisterMessage(authorize_result)
 
 
 DESCRIPTOR._options = None
-_CALL_TARGET.fields_by_name['contract_id']._options = None
+_CALL_DATA.fields_by_name['contract_id']._options = None
 # @@protoc_insertion_point(module_scope)
